68032328b7818e1bf5192182f9c79f4b8b7dbd6e
Kenan Salic <kenan.salic@bloomreach.com>
2021-12-09 10:52:57 +0100

    update on skeleton

--- src/cheatsheet.js
+++ src/cheatsheet.js
@@ -2,14 +2,14 @@ export const cheatsheet = [
 
     {
         "heading": "Getting component properties (simple properties)",
-        "reactJsx": "const properties = component.getParameters();",
+        "reactJsx": "component.getParameters();",
     },
     {
-        "heading": "Getting component content (complex properties)",
-        "reactJsx": "import {getContainerItemContent} from \"@bloomreach/spa-sdk\"; \n ... \n const content = getContainerItemContent(component, page);",
+        "heading": "Getting component content (content properties)",
+        "reactJsx": "import {getContainerItemContent} from \"@bloomreach/spa-sdk\"; \n ... \n getContainerItemContent(component, page);",
     },
     {
-        'heading': "Getting the page document attributes",
-        "reactJsx": "const pagedocument = page.getDocument().getData();",
+        'heading': "Getting the page document properties",
+        "reactJsx": "page.getDocument().getData();",
     },
 ]
--- src/code-templates.js
+++ src/code-templates.js
@@ -1,42 +1,87 @@
-export const templates = {
-    "BrPage": "<BrPage ...  mapping={{..., {componentname}}}>\n" +
-        "...\n"+
-                "</BrPage>",
-    "reactJsx": "import {getContainerItemContent} from \"@bloomreach/spa-sdk\";\n" +
-        "\n" +
-        "export function {componentname}({component, page}) {\n" +
-        "\n" +
-        "    const content = getContainerItemContent(component, page);\n" +
-        "    const properties = component.getParameters()\n" +
-        "    const pagedocument = page.getDocument().getData();\n" +
-        "\n" +
-        "    return (\n" +
-        "        <div>\n" +
-        "            <h3>{component.getName()}</h3>\n" +
-        "            <pre>content: {JSON.stringify(content, null, 2)}</pre>\n" +
-        "            <pre>properties: {JSON.stringify(properties, null, 2)}</pre>\n" +
-        "            <pre>page document: {JSON.stringify(pagedocument, null, 2)}</pre>\n" +
-        "        </div>\n" +
-        "    );\n" +
-        "}",
-    "reactTsx": "import {getContainerItemContent} from \"@bloomreach/spa-sdk\";\n" +
-        "import React from \"react\";\n" +
-        "\n" +
-        "export function {componentname}({component, page}) {\n" +
-        "\n" +
-        "    const content = getContainerItemContent(component, page);\n" +
-        "    const properties = component.getParameters()\n" +
-        "    const pagedocument = page.getDocument().getData();\n" +
-        "\n" +
-        "    return (\n" +
-        "        <div>\n" +
-        "            <h3>component.getName()</h3>\n" +
-        "            <pre>content: {JSON.stringify(content, null, 2)}</pre>\n" +
-        "            <pre>properties: {JSON.stringify(properties, null, 2)}</pre>\n" +
-        "            <pre>page document: {JSON.stringify(pagedocument, null, 2)}</pre>\n" +
-        "        </div>\n" +
-        "    );\n" +
-        "}",
-    "vue": "",
-    "angular": ""
+export const codeTemplates = {
+    "reactJsx": {
+        "BrPage": `import {{componentname}} from "./components/{componentname}";
+...
+<BrPage ...  mapping={{..., {componentname}}}>
+...
+</BrPage>`,
+        "brPageHighLight": "1,3",
+        "componentHighLight": "5,6,7,12,13,14",
+        "language": "jsx",
+        "component": `/* save as /components/{componentname}.jsx */
+import {getContainerItemContent} from "@bloomreach/spa-sdk";
+
+export function {componentname}({component, page}) {
+
+    const content = getContainerItemContent(component, page);
+    const properties = component.getParameters()
+    const pagedocument = page.getDocument().getData();
+
+    return (
+        <div>
+            <h3>{component.getName()}</h3>
+            <pre>content: {JSON.stringify(content, null, 2)}</pre>
+            <pre>properties: {JSON.stringify(properties, null, 2)}</pre>
+            <pre>page document: {JSON.stringify(pagedocument, null, 2)}</pre>
+        </div>
+    );
+}`
+    },
+    "vueJs": {
+        "BrPage": `<template>
+...
+    <br-page ... :mapping="mapping">
+      ...
+    </br-page>
+...
+<template>
+<script>
+import {componentname} from "./components/{componentname}";
+..
+export default {
+  ...
+  data: () => {
+    return {
+      configuration: {
+        ...
+      },
+      mapping: {..., {componentname}},
+    };
+  }
+}
+</script>`
+        ,
+        language: "html",
+        componentHighLight: "4,5,6,16,19,22",
+        brPageHighLight: "3,9,18",
+        component:
+            `<!-- save as /components/{componentname}.vue -->
+<template>
+  <div>
+    <h3>{{ component.getName() }}</h3>
+    <pre>content: {{ JSON.stringify(content, null, 2) }}</pre>
+    <pre>properties: {{ JSON.stringify(properties, null, 2) }}</pre>
+    <pre>page document: {{ JSON.stringify(pagedocument, null, 2) }}</pre>
+  </div>
+</template>
+
+<script>
+import {getContainerItemContent} from "@bloomreach/spa-sdk";
+
+export default {
+  computed: {
+    pagedocument() {
+      return this.page.getDocument().getData();
+    },
+    content() {
+      return getContainerItemContent(this.component, this.page);
+    },
+    properties() {
+      return this.component.getParameters();
+    }
+  },
+  props: ['component', 'page']
+}
+</script>`
+    },
 }
