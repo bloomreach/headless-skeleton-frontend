f191ada9a81d8fcad7e5fa3e386788913e353a00
Kenan Salic <kenan.salic@bloomreach.com>
2021-12-15 11:13:01 +0100

    update on skeleton

--- src/code-templates.js
+++ src/code-templates.js
@@ -84,4 +84,60 @@ export default {
 }
 </script>`
     },
+    "angular": {
+        "BrPage": `import {Component} from '@angular/core';
+import {componentname} from "./components/{componentname}/{componentname}.component";
+...
+@Component({
+  ...
+  template: \`
+    <br-page [configuration]="configuration" [mapping]="mapping">
+  </br-page>\`,
+})
+export class AppComponent {
+  configuration = {...}
+  mapping = {..., {componentname}
+  }
+}`
+        ,
+        language: "tsx",
+        componentHighLight: "4,5,6,16,19,22",
+        brPageHighLight: "2,7,8,12",
+        component:
+            `<!-- ng generate component {componentname} -->
+import {Component, Input} from '@angular/core';
+import {Component as BrComponent, ContainerItem, Document, getContainerItemContent, Page} from "@bloomreach/spa-sdk";
+
+@Component({
+  selector: 'app-{componentname}',
+  template: \`
+    <div>
+      <h3>{{component.getName()}}</h3>
+      <pre>content: {{JSON.stringify(content, null, 2)}}</pre>
+      <pre>properties: {{JSON.stringify(properties, null, 2)}}</pre>
+      <pre>page document: {{JSON.stringify(pagedocument, null, 2)}}</pre>
+    </div>\`,
+})
+export class {componentname}Component {
+
+  @Input() component!: BrComponent;
+  @Input() page!: Page;
+
+  get JSON() { return JSON;}
+
+  get properties() {
+    return this.component.getParameters<any>();
+  }
+
+  get pagedocument() {
+    return this.page.getDocument<Document>()?.getData<any>();
+  }
+
+  get content() {
+    return getContainerItemContent<any>(this.component as ContainerItem, this.page);
+  }
+
+}`
+    },
+
 }
